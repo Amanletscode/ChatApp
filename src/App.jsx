50faf5ae61ad8e19cb9d5962e81ab286364734d8
Amanletscode <aman123mot@gmail.com>
2024-08-15 15:13:46 +0530

    Protected Routes set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import React, { lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedRoutes from "./components/auth/ProtectedRoutes";
 
 const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
 const Chat = lazy(() => import("./pages/Chat"));
-const Groups = lazy(()=> import("./pages/Groups"))
+const Groups = lazy(() => import("./pages/Groups"));
+
+let user = true;
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/chat/:chatId" element={<Chat />} />
-        <Route path="/groups" element={<Groups />} />
+        <Route element={<ProtectedRoutes user={user}/>}>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat/:chatId" element={<Chat />} />
+          <Route path="/groups" element={<Groups />} />
+        </Route>
+        <Route path="/login" element={
+          <ProtectedRoutes user={!user} redirect="/">
+            <Login />
+        </ProtectedRoutes>
+      } />
+
+  {/* <Route path="*" element={<Home />} /> */}
+
       </Routes>
     </BrowserRouter>
   );
