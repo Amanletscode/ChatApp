24e7834320b1bc3b6d42aeb527450ee2d1311e2e
Amanletscode <aman123mot@gmail.com>
2024-08-17 02:53:54 +0530

    Added Reusable functions and some material icons

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,10 +1,53 @@
-import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
-import React from "react";
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import {
+  Add as AddIcon,
+  Group as GroupIcon,
+  Logout as LogoutIcon,
+  Menu as MenuIcon,
+  Notifications as NotificationsIcon
+  Search as SearchIcon,
+} from "@mui/icons-material";
+import React, { useState } from "react";
 import { orange } from "../constants/color";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  const handle = () => {
-    console.log("IconButton clicked");
+  const navigate = useNavigate();
+
+  const [isMobile, setIsMobile] = useState(false);
+  const [isSearch, setIsSearch] = useState(false);
+  const [isNewGroup, setIsNewGroup] = useState(false);
+  const [isNotification, setIsNotification] = useState(false);
+
+  const handleMobile = () => {
+    setIsMobile((prev) => !prev);
+  };
+
+  const openSearch = () => {
+    setIsSearch((prev) => !prev);
+  };
+
+  const openNewGroup = () => {
+    setIsNewGroup((prev) => !prev);
+  };
+
+  const openNotification = () => {
+    setIsNotification((prev) => !prev);
+  };
+
+  const navigateToGroup = () => {
+    navigate("/groups");
+  };
+
+  const LogoutHandler = () => {
+    console.log("Logout");
   };
 
   return (
@@ -24,13 +67,61 @@ const Header = () => {
           </Typography>
 
           <Box sx={{ display: { xs: "block", sm: "none" } }}>
-            <IconButton color="inherit" onClick={handle}>
-              {/* Icon content goes here */}
+            <IconButton color="inherit" onClick={handleMobile}>
+              <MenuIcon />
             </IconButton>
+          </Box>
+
+          <Box
+            sx={{
+              flexGrow: 1,
+            }}
+          />
+
+          <Box>
+            <IconBtn
+              title={"Search"}
+              icon={<SearchIcon />}
+              onClick={openSearch}
+            />
+
+            <IconBtn
+              title={"New Group"}
+              icon={<AddIcon />}
+              onClick={openNewGroup}
+            />
+
+            <IconBtn
+              title={"Manage Groups"}
+              icon={<GroupIcon />}
+              onClick={navigateToGroup}
+            />
+
+            <IconBtn
+              title={"Logout"}
+              icon={<LogoutIcon />}
+              onClick={LogoutHandler}
+            />
+
+<IconBtn
+              title={"Notifications"}
+              icon={<NotificationsIcon/>}
+              onClick={openNotification}
+            />
           </Box>
         </Toolbar>
       </AppBar>
     </Box>
+  );
+};
+
+const IconBtn = ({ title, icon, onClick }) => {
+  return (
+    <Tooltip title={title}>
+      <IconButton color="inherit" size="large" onClick={onClick}>
+        {icon}
+      </IconButton>
+    </Tooltip>
   );
 };
 
